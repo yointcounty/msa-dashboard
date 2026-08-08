f60cb1c2a583a6b46e4b4451a01ca9d99859f698
Yoint County <jt@yointcounty.com>
2026-08-08 12:58:13 -0400

    Add auth pages and fix routes

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,4 @@
+'use client';
+export default function LoginPage() {
+  return <div className="p-8"><h1>Login (Coming Soon)</h1></div>;
+}
---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,4 @@
+'use client';
+export default function SignupPage() {
+  return <div className="p-8"><h1>Sign Up (Coming Soon)</h1></div>;
+}
