2898bd500d4369094b210969b6ccbe058ac81b3c
yointcounty <jt@yointcounty.com>
2026-08-16 10:08:50 -0400

    Enable rotating homepage photo pair

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "./home-photo-carousel.css";
 import PwaRegister from "./pwa-register";
 
 const geistSans = Geist({
